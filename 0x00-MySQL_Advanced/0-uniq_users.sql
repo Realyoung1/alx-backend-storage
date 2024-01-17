9151836b2d21e68c13464e34b9cf443fc1d20567
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2024-01-17 23:09:13 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,4 @@
--- create table 'users' with attrs (id,email,name)
-
-
+-- i created a table if noit exists usesr
 CREATE TABLE IF NOT EXIST users(
        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        email VARCHAR(255) NOT NULL UNIQUE,
